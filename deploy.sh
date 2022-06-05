fdb301acb5bbb784d34cf0ef2c0dc69294b016df
SonMarloanto <jimsonmarloanto01@gmail.com>
2022-06-06 01:41:10 +0800

    NodeJS x Mysql

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
-GOOGLE_PROJECT_ID=angular-axe-351410
+GOOGLE_PROJECT_ID=<?>
 CLOUD_RUN_SERVICE=mobile-backend-login-register
-INSTANCE_CONNECTION_NAME=angular-axe-351410:asia-southeast2:resepai12
-DB_USER=root
-DB_PASS=12345678
-DB_NAME=resepai
-
-gcloud build submit --tag gcr.io/$GOOGLE_PROJECT_ID/$CLOUD_RUN_SERVICE \
- --project=$GOOGLE_PROJECT_ID
+INSTANCE_CONNECTION_NAME=<?>
+DB_USER=<?>
+DB_PASS=<?>
+DB_NAME=<?>
 
 gcloud run deploy $CLOUD_RUN_SERVICE \
  --image gcr.io/$GOOGLE_PROJECT_ID/$CLOUD_RUN_SERVICE \
  --add-cloudsql-instances $INSTANCE_CONNECTION_NAME \
- --update-env-vars INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME, DB_PASS=$DB_PASS, DB_USER=$DB_USER, DB_NAME=$deploy
+ --update-env-vars INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,DB_PASS=$DB_PASS,DB_USER=$DB_USER,DB_NAME=$DB_NAME \
  --platform managed \
  --region asia-southeast2 \
  --allow-unauthenticated \
--- getToken.js
+++ getToken.js
@@ -4,4 +4,4 @@ function getToken (username) {
 return 
 jwt.sign(username, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"})
 }
-module.exports=getToken
+module.exports=getTokenchmod 
